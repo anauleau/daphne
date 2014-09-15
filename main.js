ccaa9176fa0452d652d7a207a71f57a9e10c688d
Andreas Nauleau <andreas@adap.tv>
2014-09-15 09:01:08 -0700

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 //le custom js
+var daphne = angular.module('daphne', ['ngRoute']);
+
+daphne.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html'
+        })
+        .when('/contact', { 
+            templateUrl: 'views/contact.html'
+        })
+        .when('/projects', {
+            templateUrl: 'views/projects.html'
+        })
+        .whe
+});
